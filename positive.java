6e1835ea0480e0837d189000cb4ec87fb1d45e51
MUTHUKIRUTHIKA <kiruthikabe22@gmail.com>
2016-09-01 18:18:22 +0530

    Create positive.java

---
+++ positive.java
@@ -0,0 +1,13 @@
+int a=22;
+if(a>0)
+{
+System.out.println("the number is postive");
+}
+else if(a<0)
+{
+System.out.println("the number is negative");
+}
+else
+{
+System.out.println(" the number is zero);
+}
